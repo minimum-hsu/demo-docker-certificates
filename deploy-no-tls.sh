1555658c758e97aa9dc7fe22e3ff18725386c63e
Minimum <s5550055@yahoo.com.tw>
2016-08-19 20:42:11 +0000

    deploy-no-tls uses port 2375, and generate_certs uses port 2376.

---
+++ deploy-no-tls.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+set -e
+
+# Configurate Docker daemon
+sudo mkdir -p /etc/docker
+sudo cp ./daemon.json /etc/docker/daemon.json
+
+# Restart Docker daemon
+sudo service docker restart
--- generate_certs.sh
+++ generate_certs.sh
@@ -7,7 +7,7 @@ export PUBLIC_IP=$(hostname -I | awk -F " " '{ print $1 }')
 export DOMAIN=$(hostname)
 export SUBJECT=/C=TW/ST=Taiwan/L=Taipei/O=Demo/OU=Demo
 export SUBJECT_ALT_NAME=IP:$PUBLIC_IP,IP:127.0.0.1
-if [ -n $1 ] ; then
+if [ -n "$1" ] ; then
   export SUBJECT_ALT_NAME=$SUBJECT_ALT_NAME,IP:$1
 fi
 
@@ -17,21 +17,18 @@ echo subjectAltName is $SUBJECT_ALT_NAME
 # Generate server certificates
 export ca_dir=./certs/demo.com
 export certs=/var/lib/docker/certs
-mkdir -p $certs
+sudo mkdir -p $certs
 export tmp=`mktemp -d`
 trap "rm -rf $tmp" EXIT
-openssl genrsa -out $certs/server.key 4096
+sudo openssl genrsa -out $certs/server.key 4096
 openssl req -new -key $certs/server.key -out $tmp/server.csr -subj "/CN=$DOMAIN$SUBJECT"
 echo subjectAltName = $SUBJECT_ALT_NAME > $tmp/extfile.cnf
-openssl x509 -req -days 3650 -sha256 -in $tmp/server.csr -CA $ca_dir/ca.crt -CAkey $ca_dir/ca.key -CAcreateserial -out $certs/server.crt -extfile $tmp/extfile.cnf
-cp $ca_dir/ca.crt $certs/ca.crt
+sudo openssl x509 -req -days 3650 -sha256 -in $tmp/server.csr -CA $ca_dir/ca.crt -CAkey $ca_dir/ca.key -CAcreateserial -out $certs/server.crt -extfile $tmp/extfile.cnf
+sudo cp $ca_dir/ca.crt $certs/ca.crt
 
 # Configurate Docker daemon
-mkdir -p /etc/docker
-cp ./daemon.json /etc/docker/daemon.json
+sudo mkdir -p /etc/docker
+sudo cp ./daemon-tls.json /etc/docker/daemon.json
 
 # Restart Docker daemon
-systemctl daemon-reload
-systemctl restart docker
-systemctl enable docker
-
+sudo service docker restart
